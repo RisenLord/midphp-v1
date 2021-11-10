d855434f2d4f2883305236455e1cdf8425be47bb
RisenLord <risenlord7@gmail.com>
2021-11-10 06:39:14 +0300

    last but not least

--- 16_sessions_cookies/01_page_counter.php
+++ 16_sessions_cookies/01_page_counter.php
@@ -1,13 +1,45 @@
-<!doctype html>
+<?php
+session_start();
+
+// echo session_id();
+
+// $_SESSION['name'] = 'Lord';
+
+// echo '<pre>';
+// var_dump($_SESSION);
+// echo '</pre>';
+
+// unset($_SESSION['name']);
+
+// session_unset();
+
+$_SESSION['page_counter'] = $_SESSION['page_counter'] ?? 0;
+$_SESSION['page_counter']++;
+
+$pageCounter = $_SESSION['page_counter'] ?? 0;
+if ($_SESSION['page_counter'] === 10) {
+    echo 'Thanks for visiting us 10x' . '<br>';
+    session_unset();
+    session_destroy();
+}
+
+?>
+
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h1>My Awesome page></h1>
+    <i>
+        <p>visited <?php echo $pageCounter ?> times</p>
+    </i>
+    <h1>Awesome</h1>
 </body>
+
 </html>
